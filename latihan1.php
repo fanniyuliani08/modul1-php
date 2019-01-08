feefba2614ef6d7b21cb1d633fef98ddaa100b21
fanniyuliani08 <fanni.yuliani08@gmail.com>
2019-01-08 07:28:07 +0700

    asoy

---
+++ latihan1.php
@@ -0,0 +1,5 @@
+<?php
+echo "Namaku Fanni";
+echo "<br>";
+echo "aku sedang makan bakso";
+?>
---
+++ latihan4.php
@@ -0,0 +1,8 @@
+<?php
+$nama = "Fanni";
+function tampil_nama(){
+  global $nama;
+  echo "Nama Saya : ".$nama;
+}
+tampil_nama();
+ ?>
